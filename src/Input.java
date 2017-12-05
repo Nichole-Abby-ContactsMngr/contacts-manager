0752cfb83330dbfe111fa1c661770e301c6becfa
nicholekirlin <nichole.kirlin@gmail.com>
2017-12-05 14:57:39 -0600

    working on getting a contact to add

---
+++ src/Input.java
@@ -0,0 +1,116 @@
+import java.util.Scanner;
+
+public class Input {
+    private Scanner scan;
+
+    public Input() {
+        this.scan = new Scanner(System.in);
+    }
+
+    public String getString() {
+        return this.scan.nextLine();
+    }
+
+    public String getString(String prompt) {
+        System.out.println(prompt);
+        return this.scan.nextLine();
+    }
+//
+//
+//    public boolean yesNo() {
+//        String input = this.scan.next();
+//        if(input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes")) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//
+//    public boolean yesNo(String prompt) {
+//        System.out.println(prompt);
+//        String input = this.scan.next();
+//        if(input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes")) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//
+//    public int getInt(int min, int max) {
+//        System.out.println("Please input an integer between " + min + " and " + max);
+//
+//        int userInput = this.getInt();
+//
+//        if(userInput > max || userInput < min) {
+//            return getInt(min, max);
+//        } else {
+//            return userInput;
+//        }
+//    }
+//
+//    public int getInt(int min, int max, String prompt) {
+//        System.out.println(prompt);
+//
+//        int userInput = this.getInt();
+//
+//        if(userInput > max || userInput < min) {
+//            return getInt(min, max);
+//        } else {
+//            return userInput;
+//        }
+//    }
+////
+////    public int getInt() {
+////        String userInput = this.scan.next();
+////        int userInt = Integer.valueOf(userInput);
+////        try {
+////            return Integer.valueOf(userInput);
+////        } catch (NumberFormatException e) {
+////            System.out.println("Your input must be a number");
+////            System.out.println(e.getMessage());
+////            return.getInt();
+////        }
+////    }
+//
+//    public int getInt(String prompt) {
+//        System.out.println(prompt);
+//        return this.scan.nextInt();
+//    }
+//
+//    public double getDouble(double min, double max) {
+//        System.out.println("Please input an decimal number between " + min + " and " + max);
+//
+//        double userInput = this.getDouble();
+//
+//        if(userInput > max || userInput < min) {
+//            return getDouble(min, max);
+//        } else {
+//            return userInput;
+//        }
+//    }
+//
+//    public double getDouble(double min, double max, String prompt) {
+//        System.out.println(prompt);
+//
+//        double userInput = this.getDouble();
+//
+//        if(userInput > max || userInput < min) {
+//            return getDouble(min, max);
+//        } else {
+//            return userInput;
+//        }
+//    }
+//
+//    public double getDouble() {
+//
+//    }
+//
+//    public double getDouble() {
+//        return this.scan.nextDouble();
+//    }
+//
+//    public double getDouble(String prompt) {
+//        System.out.println(prompt);
+//        return this.scan.nextDouble();
+//    }
+}
--- src/contactsapp.java
+++ src/contactsapp.java
@@ -1,31 +1,68 @@
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class contactsapp {
 
     public static void main(String[] args) {
         System.out.println("*****Welcome to Contacts Manager*****");
         System.out.println("[Main Menu] Press: A Add Contact - S Search Contact - D Delete");
-    }
 
-        public static void contactsToFile() throws IOException {
-            String directory = "contacts-manager";
-            String filename = "contacts.txt";
-            Path dataDirectory = Paths.get(directory);
-            Path dataFile = Paths.get(directory, filename);
+        Input userResponse = new Input();
+        userResponse.getString("Enter a phone number");
 
-            if (Files.notExists(dataDirectory)) {
-                Files.createDirectories(dataDirectory);
-            }
 
-            if (!Files.exists(dataFile)) {
-                Files.createFile(dataFile);
-            }
-
+        try {
+            writeContactsToFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            System.out.println("IO Exception, fix your stuff!");
+        } catch (Exception e) {
+            System.out.println(e.getStackTrace());
+            System.out.println(e.getMessage());
+            System.out.println("Something went wrong!");
         }
 
+        readAllContacts();
     }
 
+    public static void writeContactsToFile() throws IOException {
+        String directory = "data";
+        String filename = "contacts.txt";
+        Path dataDirectory = Paths.get(directory);
+        Path dataFile = Paths.get(directory, filename);
 
+        if (Files.notExists(dataDirectory)) {
+            Files.createDirectories(dataDirectory);
+        }
+
+        if (! Files.exists(dataFile)) {
+            Files.createFile(dataFile);
+        }
+
+        ArrayList<String> contacts = new ArrayList<>();
+//        contacts.add();
+
+        Files.write(dataFile, contacts, StandardOpenOption.APPEND);
+    }
+
+    public static void readAllContacts() {
+        String directory = "data";
+        String filename = "contacts.txt";
+        Path dataDirectory = Paths.get(directory);
+        Path dataFile = Paths.get(directory, filename);
+        List<String> contacts;
+
+        try {
+            contacts = Files.readAllLines(dataFile);
+            for(String contact : contacts) {
+                System.out.println(contacts);
+            }
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
